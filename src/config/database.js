333950697b0298b34422925e028cd74da577ba20
doctorzik <ezekieljeremiah92@gmail.com>
2026-02-23 22:32:14 +0100

    refactor: update database connection and tsconfig settings

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,6 @@
 import 'dotenv/config';
 import { PrismaPg } from '@prisma/adapter-pg';
-import pkg from '../generated/prisma/client.js';
-
-const { PrismaClient } = pkg;
+import { PrismaClient } from '@prisma/client';
 const connectionString = `${process.env.DATABASE_URL}`;
 
 const adapter = new PrismaPg({ connectionString });
